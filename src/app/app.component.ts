f63c314cb7ff606b395b3bb0ccd89c15ddf06d74
eswaribala <parameswaribala@gmail.com>
2022-03-22 08:32:26 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bank-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bankingapp';
+  logo='../assets/images/logo.jpg'
 }
