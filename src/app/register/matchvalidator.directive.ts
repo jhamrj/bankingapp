434adca07d75af38517c8b10e3acede7038aa6f6
eswaribala <parameswaribala@gmail.com>
2022-03-29 08:12:23 +0530

    updated

--- src/app/register/matchvalidator.directive.ts
+++ src/app/register/matchvalidator.directive.ts
@@ -1,8 +1,9 @@
 import {Attribute, Directive} from '@angular/core';
-import {AbstractControl, ValidationErrors, Validator} from "@angular/forms";
+import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from "@angular/forms";
 
 @Directive({
-  selector: '[appMatchvalidator]'
+  selector: '[appMatchvalidator][formControlName],[appMatchvalidator][formControl],[appMatchvalidator][ngModel]',
+  providers:[{provide: NG_VALIDATORS, useExisting: MatchValidatorDirective, multi: true}]
 })
 export class MatchValidatorDirective implements Validator{
 
@@ -11,11 +12,11 @@ export class MatchValidatorDirective implements Validator{
   validate(control: AbstractControl): ValidationErrors | null {
     // self value (e.g. retype password)
     let v = control.value;
-
+    console.log(v)
     // control value (e.g. password)
     //confirmpassword--->form--> get password
     let e = control.root.get(this.appMatchvalidator);
-
+    console.log(e);
     // value not equal
     if (e && v !== e.value) return {
       validateEqual: false
