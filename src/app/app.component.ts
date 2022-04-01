2b6b8f276baf07e7aae1223e62e5723d28a06cdd
eswaribala <parameswaribala@gmail.com>
2022-04-01 08:20:48 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {ConfigService} from "./config.service";
 import {AuthService} from "./services/auth.service";
-
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, Event} from "@angular/router";
+import {Observable} from "rxjs/index";
+import {filter, map, take} from "rxjs/operators";
 @Component({
   selector: 'bank-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit{
 
   logo:any;
@@ -14,8 +17,13 @@ export class AppComponent implements OnInit{
   title:any;
   token_key:any;
   auth_user:any;
+  loading = true;
+  readonly loading$: Observable<boolean> = this.router.events.pipe(
+    map((event) => this.checkRouterEvent(event))
+  );
   //dependency injection
-  constructor(private configService:ConfigService,private authService:AuthService) {
+  constructor(private configService:ConfigService,
+              private authService:AuthService,private router:Router) {
   }
 
   ngOnInit(): void {
@@ -27,5 +35,21 @@ export class AppComponent implements OnInit{
         console.log(this.token_key,this.auth_user);
     }
 
+  checkRouterEvent(routerEvent: Event): boolean {
+    let status:boolean = false;
+    if (routerEvent instanceof NavigationStart) {
+      status=true;
+      console.log(status);
+    }
+
+    if (routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationError) {
+      status=false
+      console.log(status);
+    }
+    console.log(status);
+    return status;
+  }
 
 }
